2d59861888e0cff0ba1bb850dd78ba35309b0a91
UP959856 <mwverdon2000@gmail.com>
2022-06-17 16:20:06 +0100

    Exception Handling

--- randomword.py
+++ randomword.py
@@ -7,16 +7,21 @@ def chooseWord():
     words_src = "https://www.mit.edu/~ecprice/wordlist.10000"
     randomWord = ""
 
+    try:
     #Only picks words with a length of at least 6
-    while (len(randomWord) < 6):
+        while (len(randomWord) < 6):
 
-        #request to get words from webpage
-        response = requests.get(words_src)
+            #request to get words from webpage
+            response = requests.get(words_src)
 
-        #Get the word list and parse by newlines
-        wordsList = response.content.splitlines()
+            #Get the word list and parse by newlines
+            wordsList = response.content.splitlines()
         
-        #Pick a random word 
-        randomWord = random.choice(wordsList).lower().decode("utf-8")
+            #Pick a random word 
+            randomWord = random.choice(wordsList).lower().decode("utf-8")
+
+    except TimeoutError:
+
+        print("Make sure you have wifi connection")
     
     return randomWord
