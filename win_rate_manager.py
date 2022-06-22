0cb708a6911fedfdf586fca253132749f720c939
UP959856 <mwverdon2000@gmail.com>
2022-06-23 00:47:11 +0100

    Added object pickling to save winrate data

--- win_rate_manager.py
+++ win_rate_manager.py
@@ -1,5 +1,5 @@
-import json
 import winrate_format
+import pickle
 
 #This class handles calculating winrates and saving it as well as loading them.
 class win_rate_manager:
@@ -8,44 +8,67 @@ class win_rate_manager:
 
         self.difficulty = difficulty
         self.wonGame = wonGame
-        self.file = "winrate.json"
+        self.file = "winrate.pickle"
 
     #Called by other functions in class, not invoked otherwise
-    def openFile(self):
-
-        with open(self.file, 'r') as f:
-            fileContents = json.load(f)
-            return fileContents
-    
-    #Resets file to original format
     def resetFile(self):
 
-        baseContents = winrate_format.getFormat()
+        with open(self.file,"wb") as file:
+
+            dumpContents = (winrate_format.getFormat())
+            pickle.dump(dumpContents, file)
+
+    #Open file and view contents
+    def openFile(self):
+
+        with open(self.file,'rb') as file:
+
+            contents = pickle.load(file)
+
+            return contents
+
+    #Opens file and updates the winrate
+    def updateWinRate(self):
+
+        winrateData = self.openFile()
+
+        #Iterate through the list of dictionaries containing winrate data.
+        for data in winrateData:
+
+            if(data["difficulty"] == self.difficulty):
+
+                data = self.calculateWinRate(data)
+
+        with open(self.file,'wb') as file:
+            pickle.dump(winrateData, file)
+
+    def calculateWinRate(self, dict):
+
+        #Increment games by 1 as game was played when this is called.
+        dict["games"] += 1
+        games = dict["games"]
+        gamesWon = dict["wins"]
         
-        with open(self.file, 'w') as f:
-            json.dump(baseContents, f)
+        #Increment wins by 1 is game is won
+        if (self.wonGame):
 
-    #Updates the win rate given the difficulty played on and if the game was won.
-    def updateWinRate(self, difficulty, won):
+            gamesWon += 1
+            dict["wins"] = gamesWon
 
-        contents = self.openFile()
-        for i, k in contents.items():
+        #Update new winrate
+        newWinrate = (gamesWon/games) * 100
+        dict["winrate"] = newWinrate
 
-            print(i, k + "\n")
-
-        json.dumps(contents)
-
-        
+        return dict
 
 
-        
 
-newJson = win_rate_manager(2, True)
-newJson.resetFile()
-#newJson.updateWinRate('Easy', True)
-string = newJson.openFile()
-print(type(string))
-print(string)
+newPickle = win_rate_manager("medium", True)
+newPickle.resetFile()
+pickleContents = newPickle.openFile()
+print(pickleContents)
+    
+
         
 
 
--- winrate_format.py
+++ winrate_format.py
@@ -1,32 +1,9 @@
 def getFormat():
 
-    baseContents = {
-            'Winrates' : [
-                {
-                    'difficulty' : 'easy',
-                    'games' : 0,
-                    'wins' : 0,
-                    'winrate' : 0.0
-                },
-                {
-                    'difficulty' : 'medium',
-                    'games' : 0,
-                    'wins' : 0,
-                    'winrate' : 0.0
-                },
-                {
-                    'difficulty' : 'hard',
-                    'games' : 0,
-                    'wins' : 0,
-                    'winrate' : 0.0
-                },
-                {
-                    'difficulty' : 'lol',
-                    'games' : 0,
-                    'wins' : 0,
-                    'winrate' : 0.0
-                }
-            ]
-        }
+    baseContents = [{'difficulty' : 'easy', 'games' : 0, 'wins' : 0,'winrate' : 0.0},
+                {'difficulty' : 'medium', 'games' : 0, 'wins' : 0, 'winrate' : 0.0},
+                {'difficulty' : 'hard', 'games' : 0, 'wins' : 0, 'winrate' : 0.0},
+                {'difficulty' : 'lol', 'games' : 0, 'wins' : 0, 'winrate' : 0.0}]
+        
 
     return baseContents
